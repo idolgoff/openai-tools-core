c48796904399410c69d8ef15aa672c833f55a45d
Ivan Dolgov <indolgov@spb.beeline.ru>
2025-03-17 19:58:46 +0500

    build: some versioning setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai_tools_core",
-    version="0.1.0",
+    version="0.2.0",
     description="Core library for AI tools and integrations",
     author="idolgoff",
     packages=find_packages(where="src"),
